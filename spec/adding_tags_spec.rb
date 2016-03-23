46342f93261ceeee8f4f785e2597e6e18da0a406
Murilo Dal Ri <murilodalri@gmail.com>
2016-03-23 18:05:14 +0000

    multiple tags now working

--- spec/adding_tags_spec.rb
+++ spec/adding_tags_spec.rb
@@ -13,4 +13,15 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('education')
   end
 
+  scenario 'I can add a multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.andrew.com/'
+    fill_in 'title', with: 'Andrew'
+    fill_in 'tags',  with: 'bubbles, comma'
+    click_button 'Submit link'
+    link = Link.last
+    expect(link.tags.map(&:name)).to include('bubbles', 'comma')
+  end
+
+
 end
