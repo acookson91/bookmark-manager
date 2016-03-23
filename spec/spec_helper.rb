0440a688cf893d4ffbba74d1094ba57d31a75853
Andrew Cookson <acookson91@gmail.com>
2016-03-23 11:21:57 +0000

    Updated servers with heroku info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ Capybara.app = Bookmarks
 # will add to the boot time of your test suite on EVERY test run, even for an
 # individual file that may not need all of that loaded. Instead, consider making
 # a separate helper file that requires the additional dependencies and performs
-# the additional setup, and require it from the spec files that actually need
+# the additional setup, and require it from the spebc files that actually need
 # it.
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
